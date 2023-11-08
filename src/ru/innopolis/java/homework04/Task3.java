b1ad06e8efc1a6224e5554d28dabd6e2e89525da
SvejiKvas <uhddyprogg@gmail.com>
2023-11-08 18:56:15 +0300

    Класс   String  (темы«Классы: поля, свойства, методы» и «Класс Object. Класс String»).

---
+++ src/ru/innopolis/java/homework04/Task3.java
@@ -0,0 +1,36 @@
+package main.java.fourthHomework;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Task3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String[] strings = scan.nextLine().split(" ");
+
+        System.out.printf("Исправленная строка: %s", repairWords(strings));
+    }
+
+    static String repairWords (String[] words) {
+        StringBuilder sb = new StringBuilder();
+
+        String[] wordOne = words[0].split("");
+        Arrays.sort(wordOne);
+        String[] wordTwo = words[1].split("");
+        Arrays.sort(wordTwo);
+
+        for (String s : wordOne) {
+            sb.append(s);
+        }
+
+        sb.append(" ");
+
+        for (String s : wordTwo) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
+
