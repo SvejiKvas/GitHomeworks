e10acd1f47fa7843b1f1ed966c9ede885099c8a9
SvejiKvas <uhddyprogg@gmail.com>
2023-11-08 21:51:09 +0300

    Added an additional task, but without sorting by the channel number.

--- src/ru/innopolis/java/homework03/App.java
+++ src/ru/innopolis/java/homework03/App.java
@@ -1,11 +1,34 @@
 package ru.innopolis.java.homework03;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        TV oled = new TV("Samsung", 51, 144, true);
-        TV lg = new TV("LG", 32);
+        Scanner scan = new Scanner(System.in);
+        Random rand = new Random();
+        String[] namesMas = new String[] {
+                "LG", "Sony", "Panasonic", "Philips", "Samsung",
+                "TCL", "Hisense", "Grundig", "Huawei", "Motorola"}; // список чтобы не тестировать код долго
 
-        System.out.println(oled.toString());
-        System.out.println(lg.toString());
+        TV[] classMas = new TV[10];
+
+        for (int i = 0; i < 10; i++) {
+            classMas[i] = new TV(
+                    namesMas[i],
+                    rand.nextInt(0, 1000),
+                    rand.nextInt(0, 95),
+                    rand.nextBoolean()
+            );
+        }
+
+        int maxVolume = scan.nextInt();
+        maxVolume = (maxVolume < 50) ? 50 : (Math.min(maxVolume, 70));
+
+        for (TV el: classMas) {
+            if (el.isOn() && el.getVolume() <= maxVolume) {
+                System.out.println(el.getBrand() + "'s volume: " + el.getVolume());
+            }
+        }
     }
 }
--- src/ru/innopolis/java/homework03/TV.java
+++ src/ru/innopolis/java/homework03/TV.java
@@ -6,16 +6,32 @@ public class TV {
     private int refreshRate;
     private boolean have4kDisplay;
 
+    private int activeChannelNumber;
+    private int volume;
+    private boolean isOn;
+
     public TV() {
+    }
+
+    public TV (String brand,
+               int activeChannelNumber,
+               int volume,
+               boolean isOn) {
+        this.brand = brand;
+        this.activeChannelNumber = activeChannelNumber;
+        this.volume = volume;
+        this.isOn = isOn;
     }
 
     public TV(String brand,
               int diagonal,
               int refreshRate,
+              int volume,
               boolean have4kDisplay) {
         this.brand = brand;
         this.diagonal = diagonal;
         this.refreshRate = refreshRate;
+        this.volume = volume;
         this.have4kDisplay = have4kDisplay;
     }
     public TV(String brand,
@@ -24,6 +40,60 @@ public class TV {
         this.diagonal = diagonal;
         this.refreshRate = 60;
         this.have4kDisplay = false;
+    }
+
+
+    public String toString() {
+        String className = this.getClass().getName();
+        String display4kPointer = this.have4kDisplay ? "Yes" : "No";
+        String isOnPointer = this.isOn ? "Yes" : "No";
+        return String.format(
+                    "Class name: %s%n" +
+                    "Brand: %s%n" +
+                    "Diagonal: %s%n" +
+                    "RefreshRate: %s%n" +
+                    "Have 4k display: %s%n" +
+                    "Active channel number: %s%n" +
+                    "Volume: %s%n" +
+                    "TV is on: %s%n",
+                className,
+                this.brand,
+                this.diagonal,
+                this.refreshRate,
+                display4kPointer,
+                activeChannelNumber,
+                volume,
+                isOnPointer);
+    }
+
+    @Override
+    public boolean equals (Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        return
+                this.brand.equals(((TV) obj).brand) &&
+                this.diagonal == ((TV) obj).diagonal &&
+                this.refreshRate == ((TV) obj).refreshRate &&
+                this.have4kDisplay == ((TV) obj).isHave4kDisplay() && this.activeChannelNumber == ((TV) obj).activeChannelNumber &&
+                this.volume == ((TV) obj).volume &&
+                this.isOn == ((TV) obj).isOn;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 31;
+        result = result * 17 + brand.hashCode();
+        result = result * 17 + diagonal;
+        result = result * 17 + refreshRate;
+        result = result * 17 + (have4kDisplay ? 1 : 0);
+        result = result * 17 + activeChannelNumber;
+        result = result * 17 + volume;
+        result = result * 17 + (isOn ? 1 : 0);
+        return result;
     }
 
     public String getBrand() {
@@ -46,9 +116,30 @@ public class TV {
         return have4kDisplay;
     }
 
-    public String toString() {
-        String display4kPointer = this.have4kDisplay ? "Yes" : "No";
-        return String.format("Brand: %s%nDiagonal: %s%nRefreshRate: %s%nHave 4k display: %s%n",
-                this.brand, this.diagonal, this.refreshRate, display4kPointer);
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        if (volume <= 100 && volume >= 0) {
+            this.volume = volume;
+        } else if (volume > 100) {this.volume = 100;}
+        else {this.volume = 0;}
+    }
+
+    public int getActiveChannelNumber() {
+        return activeChannelNumber;
+    }
+
+    public void setActiveChannelNumber(int activeChannelNumber) {
+        this.activeChannelNumber = activeChannelNumber;
+    }
+
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public void setOn(boolean on) {
+        isOn = on;
     }
 }
