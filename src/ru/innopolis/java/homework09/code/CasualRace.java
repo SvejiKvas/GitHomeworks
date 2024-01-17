bcf4e3ac71d6877892e97f926003b9b96d8f0e99
SvejiKvas <uhddyprogg@gmail.com>
2024-01-17 20:07:47 +0300

    Переопределил equals, toSttring и hashCode, методы выполняют то же что и в родительском классе

---
+++ src/ru/innopolis/java/homework09/code/CasualRace.java
@@ -0,0 +1,32 @@
+package ru.innopolis.java.homework09.code;
+
+public class CasualRace extends Race {
+
+    static {
+        System.out.println("Обычная гонка");
+    }
+
+    CasualRace() {
+        this.route = "unnamed";
+        this.participants = new Car[] {};
+    }
+    CasualRace (int length, String route, int prizePool, Car[] participants) {
+        super(length, route, prizePool, participants);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ src/ru/innopolis/java/homework09/code/DragRace.java
@@ -0,0 +1,31 @@
+package ru.innopolis.java.homework09.code;
+
+public class DragRace extends Race{
+    static {
+        System.out.println("Гонка за самый мощный двигатель. Идеальное переключениепередач — залог победы!");
+    }
+
+    DragRace() {
+        this.route = "unnamed";
+        this.participants = new Car[] {};
+    }
+    DragRace (int length, String route, int prizePool, Car[] participants) {
+        super(length, route, prizePool, participants);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
